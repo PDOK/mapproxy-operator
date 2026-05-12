54d2a4685ab15134ebb374660ed6a6b9a42b9779
Léon van der Kaap <leonkaap@gmail.com>
2026-05-12 10:58:05 +0200

    Trigger CI/CD

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -146,7 +146,7 @@ var _ = Describe("Manager", Ordered, func() {
 			verifyControllerUp := func(g Gomega) {
 				// Get the name of the controller-manager pod
 				cmd := exec.Command("kubectl", "get",
-					"pods",
+					"pods", "-l", "control-plane=controller-manager",
 					"-o", "go-template={{ range .items }}"+
 						"{{ if not .metadata.deletionTimestamp }}"+
 						"{{ .metadata.name }}"+
